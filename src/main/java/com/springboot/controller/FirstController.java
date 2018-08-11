bdbc2b0e9cf6b97f3102a5764e67b0a759c95553
suryakommula777 <suryaabcdefg777@gmail.com>
2018-08-11 19:32:32 +0530

    First Commit

---
+++ src/main/java/com/springboot/controller/FirstController.java
@@ -0,0 +1,14 @@
+package com.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FirstController {
+	@RequestMapping("/hi")
+	String home(ModelMap modal) {
+		System.out.println("Entered into Controller..!");
+		return "hello";
+	}
+}
